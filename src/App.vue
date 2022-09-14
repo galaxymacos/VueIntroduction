616f999ba0af1028b83070b1e65fe2f14f5ed59c
xunruan <xunruan@icloud.com>
2022-09-14 14:06:22 -0400

    Use watch to update responsively
    watch;
    async, await;

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
-<script setup>
-import { ref, onMounted } from 'vue'
 
-const p = ref(null) // p must match the ref name in the template
-onMounted(() => {
-  p.value.textContent = 'Hello World'
-})
+
+<script setup>
+import {ref, watch} from 'vue'
+const id = ref(1)
+const todoData = ref(null)
+async function fetchData() {  // async function
+  todoData.value = null
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id.value}`) // wait for fetch
+  todoData.value = await res.json() // await all functions in the chain (source is not immediately available)
+}
+fetchData()
+watch(id, fetchData)  // watch id and call fetchData when id changes
 </script>
 
 <template>
-<!--  ref = a reference to an element in the template to be used in the script setup-->
-  <p ref="p"></p>
+  <p>Todo id: {{ id }}</p>
+  <button @click="id++">Fetch new data</button>
+<!--  show loading when todoData is null (set in script setup)-->
+  <p v-if="!todoData">Loading</p>
+  <pre v-else>{{ todoData }}</pre>
 </template>
