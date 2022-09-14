f8cbd6a609910ae45e17cae6101f2a80f23881e1
xunruan <xunruan@icloud.com>
2022-09-14 14:56:21 -0400

    pass element from parent to child
    defineProps
    import other component as child

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
-
-
 <script setup>
-import {ref, watch} from 'vue'
-const id = ref(1)
-const todoData = ref(null)
-async function fetchData() {  // async function
-  todoData.value = null
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id.value}`) // wait for fetch
-  todoData.value = await res.json() // await all functions in the chain (source is not immediately available)
-}
-fetchData()
-watch(id, fetchData)  // watch id and call fetchData when id changes
+import { ref } from 'vue'
+import ChildComp from './components/ChildComp.vue'  // Import the child component
+
+const greeting = ref('Hello from parent')
 </script>
 
 <template>
-  <p>Todo id: {{ id }}</p>
-  <button @click="id++">Fetch new data</button>
-<!--  show loading when todoData is null (set in script setup)-->
-  <p v-if="!todoData">Loading</p>
-  <pre v-else>{{ todoData }}</pre>
+<!--  Pass the greeting variable to the child component, the name "msg" must be declared in the defineProps in child -->
+  <ChildComp :msg="greeting" />
 </template>
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps({ // A child component can accept input from the parent component using props.
+  msg: String
+})
+</script>
+
+<template>
+  <h2>{{ msg || 'No props passed yet' }}</h2>
+</template>
