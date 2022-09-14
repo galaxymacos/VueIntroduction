33bd8d52fe03225ae94be59862795d8063dbf2e4
xunruan <xunruan@icloud.com>
2022-09-14 00:49:40 -0400

    Use v-if and v-else to control condition workflow

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <!--Vue composition API-->
 
 <script setup>
-import {ref} from "vue";
-const text = ref("");
-function onInput(e) {
-  text.value = e.target.value;
+import { ref } from 'vue'
+
+const awesome = ref(true)
+
+function toggle() {
+  awesome.value = !awesome.value
 }
 </script>
 
 <template>
-  <input :value="text" @input="onInput" placeholder="Type here">
-  <p>{{ text }}</p>
+  <button @click="toggle">toggle</button>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
 </template>
