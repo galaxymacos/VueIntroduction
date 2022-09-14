302770537817a327a2a944078c9781e4110c1dc9
xunruan <xunruan@icloud.com>
2022-09-14 13:38:29 -0400

    use onMounted to initialize element in template
    ref
    onMounted
    DOM

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,13 @@
 <script setup>
-import {computed, ref} from 'vue'
-const newTodo = ref('')
-let id = 0
-const todos = ref([
-  { id: id++, text: 'Learn Vue 3', done: true },
-  { id: id++, text: 'Learn Vue 3 Composition API', done: false },
-  { id: id++, text: 'Build something awesome', done: false },
-])
-// Computed properties
-const filteredTodos = computed(() => {
-  if(hideDone.value) {
-    return todos.value.filter(todo => !todo.done)
-  }
-  else{
-    return todos.value
-  }
+import { ref, onMounted } from 'vue'
+
+const p = ref(null) // p must match the ref name in the template
+onMounted(() => {
+  p.value.textContent = 'Hello World'
 })
-
-const hideDone = ref(false)
-function addTodo() {
-  todos.value.push({
-    id: id++,
-    text: newTodo.value,
-    done: false,
-  })
-}
-
-function removeTodo(todo) {
-  todos.value.splice(todos.value.indexOf(todo), 1)
-}
 </script>
 
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo">
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-<!-- Bind bool attribute to checkbox -->
-      <input type="checkbox" v-model="todo.done">
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-<!--inline function-->
-  <button @click="hideDone = !hideDone">{{ hideDone ? 'Show Done' : 'Hide Done'}} </button>
+<!--  ref = a reference to an element in the template to be used in the script setup-->
+  <p ref="p"></p>
 </template>
-
-<style>
-.done {
-  text-decoration: line-through;
-}
-</style>
