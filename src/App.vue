216ad8ee92d22e520b735c883fdcfedb7f06ed73
xunruan <xunruan@icloud.com>
2022-09-14 00:47:40 -0400

    Two ways bindings, using v-bind and v-on
    use v-on to send data to script
    use v-bind to send data back to template

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 
 <script setup>
 import {ref} from "vue";
-const count = ref(0);
-function onClick() {
-  count.value++;
+const text = ref("");
+function onInput(e) {
+  text.value = e.target.value;
 }
 </script>
 
 <template>
-  <button @click="onClick">Count is {{count}}</button>
+  <input :value="text" @input="onInput" placeholder="Type here">
+  <p>{{ text }}</p>
 </template>
