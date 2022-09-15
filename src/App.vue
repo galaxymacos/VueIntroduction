ac75b3b7be8d275ca28341be8e23e5649d75d12a
xunruan <xunruan@icloud.com>
2022-09-14 21:22:20 -0400

    Conditionals and loops
    @click
    v-if, v-else
    v-for
    :key

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <script setup>
 import { ref } from 'vue'
-const hiddenText = ref("hidden text")
-const isRed = ref(false)
-const color = ref("green")
-
-function changeColor() {
-  if (color.value === "green") {
-    color.value = "red"
-  } else {
-    color.value = "green"
-  }
+const numbers = ref([])
+const visible = ref(true)
+function addNumber() {
+  numbers.value.push(numbers.value.length + 1)
 }
-</script>
-<template>
-<!-- directly change the value of an attribute-->
-  <p :title="hiddenText">Hover</p>
-<!--  use bool to add toggle a value on and off in the attribute -->
-  <p :class="{red: isRed}" @click="isRed = !isRed">Red or nothing</p>
-<!--  use string to toggle an attribute in style -->
-  <p :style="{color: color}" @click="changeColor">Red or green</p>
+function removeNumber() {
+  numbers.value.pop()
+}
+numbers.value.push(1)
+numbers.value.push(2)
+numbers.value.push(3)
 
+function reverse() {
+  numbers.value.reverse()
+}
+
+function toggleList() {
+  visible.value = !visible.value
+}
+
+</script>
+
+<template>
+  <button @click="toggleList">Toggle List</button>
+  <button @click="addNumber">Push Number</button>
+  <button @click="removeNumber">Pop Number</button>
+  <button @click="reverse">Reverse List</button>
+  <ul v-if="visible">
+    <li v-for="number in numbers" :key="numbers.indexOf(number)">
+      {{ number }}
+    </li>
+  </ul>
+  <p v-else>List is hidden</p>
 </template>
-<style>
- .red {
-   color: red;
- }
-</style>
